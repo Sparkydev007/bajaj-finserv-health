83348e96093c5940418ea23f0129b74b0d2d3b8e
Prathmesh <Prathmesh@gmail.com>
2026-04-20 16:48:31 +0530

    Initial Spring Boot submission

---
+++ src/main/java/com/example/bajaj/TestController.java
@@ -0,0 +1,13 @@
+package com.example.bajaj;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
